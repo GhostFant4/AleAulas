8c8a74ff899be0eb904b558622fb9c0c47054da2
GhostFant4 <152219311+GhostFant4@users.noreply.github.com>
2025-04-16 11:42:45 -0300

    Add files via upload

---
+++ Aula 8/Aula 8/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] lista = { 3, 5, 13, 7, 1899, 37, 1995, -7, 27 };
+int salva = 0;
+
+
+ for (int i = 0; lista.Length > i; i++)
+{
+    for (int j = 0; j < lista.Length; j++)
+    {
+        if (lista[j] > lista[i])
+        {
+            salva = lista[j];
+            lista[j] = lista[i];
+            lista[i] = salva;
+
+        }
+    }
+} 
+
+for (int i = 0; i < lista.Length; i++)
+{
+    Console.WriteLine(lista[i]);
+}
